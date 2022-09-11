7932ea10272ee293f897bce2b69ba957dc9a9f21
CodeWithKartikk <84595018+CodeWithKartikk@users.noreply.github.com>
2022-09-11 23:55:54 +0530

    Create FirstClass.py

---
+++ FirstClass.py
@@ -0,0 +1,49 @@
+"""Python Comments"""
+
+# Python Comments -> Comments are used to explain the python code and these aare not executable statements
+# Types of Comments
+
+# 1. Single Line Comment
+"""
+2. 
+Multi 
+Line 
+Comment
+"""
+
+# for printing the particular statement
+
+# print("Hello Python")
+
+"""Python Variables"""
+# synatax -> Declaration and Initialisation
+# variable
+
+# num1 = 7
+# num2 = 5
+# sum = num1 + num2
+# print(sum)
+
+"""Type Casting"""
+# string -> "double Quotes"
+x = int(3)
+# convert type of var from int to str
+y = str(3)
+# convert type of var from int to decimal
+z = float(3) # x will be 3.0
+
+# if we wanna chk what is the type of the var then we have to use 
+# type() function
+print(type(x))
+print(type(y))
+print(type(z))
+
+# Naming Conventions
+# Camel Case
+zomatoDeliveryPartner = "John"
+# PascalCase
+ZomatoDeliveryPartner = "EEYA"
+# SnakeCase
+zomato_delivery_partner = "Kartik"
+# SCREAM_CASE
+ZOMATO_DELIVERY_PARTNER = 3.14
